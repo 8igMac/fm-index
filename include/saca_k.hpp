2facc86ed738068511162d1dae23e64dd340ac1f
8igMac <michaelshih123@gmail.com>
2019-06-05 16:24:33 +0800

    New interface for class SACA_K and some bug fix.
    [new interface]
    make type info global by using SEQ and SA as template parameter
    of class SACA_K and further introduce SaItr, Index, SignedIndex
    and EMPTY.
    
    [bug fix]
    - cast sa_value to signed int
    - assign EMPTY into if-else statement

--- include/saca_k.hpp
+++ include/saca_k.hpp
@@ -1,19 +1,19 @@
 #pragma once
-#include <iterator>
 #include <type_traits>
-#define sgn(i) (static_cast<std::make_signed_t<SaIndex<SA_ITR>>>(i));
-const unsigned int EMPTY=((unsigned int)1)<<(sizeof(unsigned int)*8-1); 
+#include <algorithm>
 
+template<class SEQ, class SA>
 class SACA_K
 {
-    template<class ITR>
-    using SaIndex = typename std::iterator_traits<ITR>::value_type;
+    using SaItr = typename SA::iterator;
+    using Index = typename SA::value_type;
+    using SignedIndex = std::make_signed_t<Index>;
+    const Index EMPTY { ((Index)1) << (sizeof(Index)*8-1) };
 
   public:
     SACA_K() = default;
 
-    template<class SEQ, class SA>
-    void operator()(const SEQ& seq, SA& sa, typename SA::value_type k)
+    void build(const SEQ& seq, SA& sa, Index k)
     {
         return call_impl(
             seq.begin()
@@ -26,52 +26,51 @@ class SACA_K
   private:
     /// @brief Find the suffix array of seq[0..n-1] in {0..k-1}^n
     /// require seq[n-1]=0 (the sentinel!), n>=2
-    template<class SEQ_ITR, class SA_ITR>
+    template<class SEQ_ITR>
     void call_impl(
         const SEQ_ITR seq
-      , SA_ITR sa
-      , SaIndex<SA_ITR> n // seq and sa size
-      , SaIndex<SA_ITR> k // the number of character used
-      , SaIndex<SA_ITR> m // maxima available space
-      , SaIndex<SA_ITR> level = 0
+      , SaItr sa
+      , Index n // seq and sa size
+      , Index k // the number of character used
+      , Index m // maxima available space
+      , Index level = 0
     )
     {
         // stage 1: reduce the problem by at least 1/2
-        std::vector<SaIndex<SA_ITR>> bkt, count;
+        std::vector<Index> bkt(k), count(k);
         if (level == 0)
         {
             // count the number of each character
             std::for_each(seq, seq+n,
-                [](auto& chr){ count[chr]++; });
+                [&count](auto& chr){ count[chr]++; });
 
             put_lms_substr0(seq, sa, bkt, count, n);
             induce_sal0(seq, sa, bkt, count, n, false);
-            induce_sas0(saq, sa, bkt, count, n, true);
+            induce_sas0(seq, sa, bkt, count, n, false);
         }
         else
         {
             put_lms_substr1(seq, sa, n);
             induce_sal1(seq, sa, n, false);
-            induce_sas1(seq, sa, n, true);
+            induce_sas1(seq, sa, n, false);
         }
 
         // now, all the LMS-substrings are sorted and stored 
         // sparsely in SA.
-        
+
         // compact all the sorted substrings into the first n1 items
         // of SA. 2*n1 must be not larger than n (proveable)
-        SaIndex<SA_ITR> n1 = 0;
+        Index n1 = 0;
         std::for_each(sa, sa+n, 
             [&n1, &sa](auto& sa_value)
             {
-                if (sa_value > 0)
+                if (static_cast<SignedIndex>(sa_value) > 0)
                     sa[n1++] = sa_value;
             });
 
         auto sa1 = sa; // sa1: the first n1 elements in sa
         auto s1 = sa + m - n1; // s1: the last n1 element in sa
-        SaIndex<SA_ITR> name_count = 
-            name_substr(seq, sa, s1, n, m, n1, level);
+        Index name_count = name_substr(seq, sa, s1, n, m, n1, level);
 
         // stage 2: solve the reduced problem
         // recurse if names are not yet unique
@@ -79,58 +78,49 @@ class SACA_K
             call_impl(s1, sa1, n1, 0, m-n1, level+1);
         else
             std::for_each(s1, s1+n1
-                , [&sa1, i = 0](auto& elem){ sa1[elem] = i++; });
-  
+                , [&sa1, i = 0](auto& elem) mutable { sa1[elem] = i++; });
+
         // stage 3: induce SA(S) from SA(S1)
         get_sa_of_lms(seq, sa, s1, n, n1, level);
         if (level == 0)
         {
-            put_suffix0(seq, sa, bkt, count, n1);
-            induce_sal0(seq, sa, bkt, count, n, false);
+            put_suffix0(seq, sa, bkt, count, n, n1);
+            induce_sal0(seq, sa, bkt, count, n, true);
             induce_sas0(seq, sa, bkt, count, n, true);
         }
         else 
         {
             put_suffix1(seq, sa, n1);
-            induce_sal1(seq, sa, n, false);
+            induce_sal1(seq, sa, n, true);
             induce_sas1(seq, sa, n, true);
         }
     }
 
-    template<class INDEX>
     void get_buckets(
-        std::vector<INDEX>& count
-      , std::vector<INDEX>& bkt
+        std::vector<Index>& count
+      , std::vector<Index>& bkt
       , bool end
     )
     {
-        if (end)
-            std::transform(count.begin(), count.end(), bkt.begin()
-              , [sum = 0](auto& chr_count)
-                {
-                    sum += chr_count;
-                    return sum - 1;
-                });
-        else
-            std::transform(count.begin(), count.end(), bkt.begin()
-              , [sum = 0](auto& chr_count)
-                {
-                    sum += chr_count;
-                    return sum - chr_count;
-                });
-    }}
+        std::transform(count.begin(), count.end(), bkt.begin()
+          , [&end, sum = 0](auto& chr_count) mutable
+            {
+                sum += chr_count;
+                return (end) ? sum - 1 : sum - chr_count;
+            });
+    }
 
-    template<class SEQ_ITR, class INDEX>
-    auto get_lms_len(
+    template<class SEQ_ITR>
+    Index get_lms_len(
         const SEQ_ITR seq
-      , INDEX n
-      , INDEX pos
+      , Index n
+      , Index pos
     )
     {
         if (pos == n-1)
             return 1;
 
-        std::size_t dist, i = 1;
+        Index dist, i = 1;
         // 2 consecutive LMS substr must be in th form: S+L+S+L+S
         while (1)
         {
@@ -154,33 +144,34 @@ class SACA_K
         return dist + 1;
     }
 
-    template<class SEQ_ITR, class SA_ITR>
-    void name_substr(
+    template<class SEQ_ITR>
+    Index name_substr(
         const SEQ_ITR seq
-      , SA_ITR sa
-      , SA_ITR s1
-      , SaIndex<SA_ITR> n
-      , SaIndex<SA_ITR> m
-      , SaIndex<SA_ITR> n1
-      , SaIndex<SA_ITR> level
+      , SaItr sa
+      , SaItr s1
+      , Index n
+      , Index m
+      , Index n1
+      , Index level
     )
     {
         // init name array buffer
         std::fill(sa+n1, sa+n, EMPTY);
 
         // scan to compute the interim s1
-        SaIndex<SA_ITR> name, name_counter = 0;
-        SaIndex<SA_ITR> pre_pos, pre_len = 0;
+        Index name, name_counter = 0;
+        Index pre_pos, pre_len = 0;
         for (auto i = 0; i < n1; i++)
         {
             auto diff = true;
-            SaIndex<SA_ITR> pos = sa[i];
-            SaIndex<SA_ITR> len = get_lms_len(seq, n, pos);
+            Index pos = sa[i];
+            Index len = get_lms_len(seq, n, pos);
             if (len == pre_len && (pre_pos + pre_len) < n)
             {
-                for (auto j = 0; 
-                    (j < len) && (seq[pos+j] == seq[pre_pos+j]);
-                    j++);
+                Index j;
+                for (j = 0
+                  ; (j < len) && (seq[pos+j] == seq[pre_pos+j])
+                  ; j++);
                 if (j == len)
                     diff = false;
             }
@@ -188,7 +179,7 @@ class SACA_K
             if (diff)
             {
                 name = i; 
-                name_count++;
+                name_counter++;
                 sa[name] = 1; // a new name
                 pre_pos = pos;
                 pre_len = len;
@@ -218,21 +209,21 @@ class SACA_K
             cur_type = pre_type;
         }
 
-        return name_count;
+        return name_counter;
     }
 
-    template<class SEQ_ITR, class SA_ITR>
+    template<class SEQ_ITR>
     void get_sa_of_lms(
         const SEQ_ITR seq
-      , SA_ITR sa
-      , SA_ITR s1
-      , SaIndex<SA_ITR> n
-      , SaIndex<SA_ITR> n1
-      , SaIndex<SA_ITR> level
+      , SaItr sa
+      , SaItr s1
+      , Index n
+      , Index n1
+      , Index level
     )
     {
         // put LMS into s1
-        SaIndex<SA_ITR> j = n1-1;
+        Index j = n1-1;
         s1[j--] = n-1;
         bool pre_type, cur_type = false; // seq[n-2] must be L-type
         for (auto i = n-2; i > 0; i--)
@@ -255,13 +246,13 @@ class SACA_K
             std::fill(sa+n1, sa+n, 0);
     }
 
-    template<class SEQ_ITR, class SA_ITR>
+    template<class SEQ_ITR>
     void put_lms_substr0(
         const SEQ_ITR seq
-      , SA_ITR sa
-      , std::vector<SaIndex<SA_ITR>>& bkt
-      , std::vector<SaIndex<SA_ITR>>& count
-      , SaIndex<SA_ITR> n
+      , SaItr sa
+      , std::vector<Index>& bkt
+      , std::vector<Index>& count
+      , Index n
     )
     {
         // find end of each bucket
@@ -283,13 +274,13 @@ class SACA_K
         sa[0] = n-1; // set the single sentinel LMS substr
     }
 
-    template<class SEQ_ITR, class SA_ITR>
+    template<class SEQ_ITR>
     void induce_sal0(
         const SEQ_ITR seq
-      , SA_ITR sa
-      , std::vector<SaIndex<SA_ITR>>& bkt
-      , std::vector<SaIndex<SA_ITR>>& count
-      , SaIndex<SA_ITR> n
+      , SaItr sa
+      , std::vector<Index>& bkt
+      , std::vector<Index>& count
+      , Index n
       , bool suffix
     )
     {
@@ -308,13 +299,13 @@ class SACA_K
             }
     }
 
-    template<class SEQ_ITR, class SA_ITR>
+    template<class SEQ_ITR>
     void induce_sas0(
         const SEQ_ITR seq
-      , SA_ITR sa
-      , std::vector<SaIndex<SA_ITR>>& bkt
-      , std::vector<SaIndex<SA_ITR>>& count
-      , SaIndex<SA_ITR> n
+      , SaItr sa
+      , std::vector<Index>& bkt
+      , std::vector<Index>& count
+      , Index n
       , bool suffix
     )
     {
@@ -333,37 +324,37 @@ class SACA_K
             }
     }
 
-    template<class SEQ_ITR, class SA_ITR>
+    template<class SEQ_ITR>
     void put_suffix0(
         const SEQ_ITR seq
-      , SA_ITR sa
-      , std::vector<SaIndex<SA_ITR>>& bkt
-      , std::vector<SaIndex<SA_ITR>>& count
-      , SaIndex<SA_ITR> n1
+      , SaItr sa
+      , std::vector<Index>& bkt
+      , std::vector<Index>& count
+      , Index n
+      , Index n1
     )
     {
         get_buckets(count, bkt, true); // find the end of bucket
 
         // put the suffix into their bucket
-        std::for_each(std::back_inserter(sa+n1-1), sa, 
-            [&sa, &bkt, &seq](auto& sa_value)
-            {
-                sa[ bkt[seq[sa_value]]-- ] = sa_value;
-                sa_value = 0;
-            });
+        for (auto i = n1-1; i > 0; i--)
+        {
+            sa[ bkt[seq[sa[i]]]-- ] = sa[i];
+            sa[i] = 0;
+        }
         sa[0] = n-1; // set the single sentinel suffix
     }
 
-    template<class SEQ_ITR, class SA_ITR>
+    template<class SEQ_ITR>
     void put_lms_substr1(
         const SEQ_ITR seq
-      , SA_ITR sa
-      , SaIndex<SA_ITR> n
+      , SaItr sa
+      , Index n
     )
     {
         std::fill(sa, sa+n, EMPTY);
 
-        std::size_t c, pre_c = seq[n-2];
+        Index c, pre_c = seq[n-2];
         bool type, pre_type = false;
         for (auto i = n-2; i > 0; i--)
         {
@@ -375,19 +366,21 @@ class SACA_K
 
             if (type && !pre_type)
             {
-                if (sgn(sa[c]) >= 0)
+                if (static_cast<SignedIndex>(sa[c]) >= 0)
                 {
                     // SA[c] is borrowed by the right
                     //   neighbor bucket.
                     // shift-right the items in the
                     //   right neighbor bucket.
-                    std::size_t j;
-                    for (j = 1; sgn(sa[c+i]) >= 0; i++); // find counter
+                    Index j;
+                    for (j = 1
+                      ; static_cast<SignedIndex>(sa[c+i]) >= 0
+                      ; i++); // find counter
                     std::move_backward(sa+c, sa+c+j, sa+c+j+1);
                     sa[c] = EMPTY;
                 }
 
-                auto d = sgn(sa[c]);
+                auto d = static_cast<SignedIndex>(sa[c]);
                 if (d == EMPTY) // sa[c] is empty
                 {
                     if (sa[c-1] == EMPTY)
@@ -421,47 +414,50 @@ class SACA_K
         //   with its head being reused as a counter.
         for (auto i = n-1; i > 0; i--)
         {
-            auto j = sgn(sa[i]);
+            auto j = static_cast<SignedIndex>(sa[i]);
             // is sa[i] a counter?
             if (j < 0 && j != EMPTY) 
+            {
                 std::move_backward(sa+i+j, sa+i, sa+i+1);
-            sa[i+j] = EMPTY;
+                sa[i+j] = EMPTY;
+            }
         }
 
         // put the single sentinel LMS-substring.
-        SA[0] = n-1;
+        sa[0] = n-1;
     }
 
-    template<class SEQ_ITR, class SA_ITR>
+    template<class SEQ_ITR>
     void induce_sal1(
         const SEQ_ITR seq
-      , SA_ITR sa
-      , SaIndex<SA_ITR> n
+      , SaItr sa
+      , Index n
       , bool suffix
     )
     {
         for (auto i = 0, step = 1; i < n; i += step, step = 1)
         {
             auto j = sa[i] - 1;
-            if (sgn(sa[i]) <= 0)
+            if (static_cast<SignedIndex>(sa[i]) <= 0)
                 continue;
             auto c = seq[j], c1 = seq[j+1];
             bool is_L_type = (c >= c1);
             if (!is_L_type)
                 continue;
 
-            auto d = sgn(sa[c]);
+            auto d = static_cast<SignedIndex>(sa[c]);
             if (d >= 0)
             {
                 // SA[c] is borrowed by the left
                 //   neighbor bucket.
                 // shift-left the items in the
                 //   left neighbor bucket.
-                std::size_t cnt_pos;
+                Index cnt_pos;
                 // find counter
-                for (cnt_pos = c-1; 
-                    sgn(sa[cnt_pos]) >= 0 || sa[cnt_pos] == EMPTY; 
-                    cnt_pos--);
+                for (cnt_pos = c-1
+                  ; static_cast<SignedIndex>(sa[cnt_pos]) >= 0 
+                 || sa[cnt_pos] == EMPTY
+                  ; cnt_pos--);
                 std::move(sa+cnt_pos+1, sa+c+1, sa+cnt_pos);
                 if (cnt_pos < i)
                     step = 0; //TODO: why do we need step?
@@ -495,10 +491,10 @@ class SACA_K
                 sa[pos] = j;
             }
 
-            auto c2 = seq[j+2];
+            decltype(c1) c2;
             // is seq[sa[i]] L-type?
             bool is_L1 = (j+1 < n-1) && 
-                (c1 > c2 || (c1 == c2 && c1 < i)); 
+                (c1 > (c2 = seq[j+2]) || (c1 == c2 && c1 < i));
             if ((!suffix || !is_L1) && i > 0)
             {
                 auto i1 = (step == 0) ? i-1 : i;
@@ -510,41 +506,45 @@ class SACA_K
         //   with its head being reused as a counter.
         for (auto i = 1; i < n; i++)
         {
-            auto j = sgn(sa[i]);
+            auto j = static_cast<SignedIndex>(sa[i]);
             if (j < 0 && j != EMPTY) // is sa[i] a counter?
+            {
                 std::move(sa+i+1, sa+i+1-j, sa+i);
-            sa[i-j] = EMPTY;
+                sa[i-j] = EMPTY;
+            }
         }
     }
 
-    template<class SEQ_ITR, class SA_ITR>
+    template<class SEQ_ITR>
     void induce_sas1(
         const SEQ_ITR seq
-      , SA_ITR sa
-      , SaIndex<SA_ITR> n
+      , SaItr sa
+      , Index n
       , bool suffix
     )
     {
         for (auto i = n-1, step = 1; i > 0; i -= step, step = 1)
         {
             auto j = sa[i]-1;
-            if (sng(sa[i]) <= 0)
+            if (static_cast<SignedIndex>(sa[i]) <= 0)
                 continue;
             auto c = seq[j], c1 = seq[j+1];
             bool is_s_type = (c < c1) || (c == c1 && c > i);
             if (!is_s_type)
                 continue;
 
-            auto d = sgn(sa[c]);
+            auto d = static_cast<SignedIndex>(sa[c]);
             if (d >= 0)
             {
                 // SA[c] is borrowed by the right
                 //   neighbor bucket.
                 // shift-right the items in the
                 //   right neighbor bucket.
-                std::size_t cnt_pos;
+                Index cnt_pos;
                 // find counter
-                for (cnt_pos = c+1; sgn(sa[cnt_pos]) >= 0; cnt_pos++); 
+                for (cnt_pos = c+1
+                  ; static_cast<SignedIndex>(sa[cnt_pos]) >= 0
+                  ; cnt_pos++); 
                 std::move_backward(
                     sa+c
                   , sa+c+cnt_pos
@@ -555,6 +555,7 @@ class SACA_K
             }
 
             if (d == EMPTY) // sa[c] is empty
+            {
                 if (sa[c-1] == EMPTY)
                 {
                     sa[c] = -1; // init the counter
@@ -562,6 +563,7 @@ class SACA_K
                 }
                 else
                     sa[c] = j;
+            }
             else // sa[c] is reused as a counter
             {
                 auto pos = c+d-1;
@@ -596,24 +598,23 @@ class SACA_K
         if (!suffix)
             for (auto i = n-1; i > 0; i--)
             {
-                auto j = sgn(sa[i]);
+                auto j = static_cast<SignedIndex>(sa[i]);
                 if (j < 0 && j != EMPTY) // is sa[i] a counter?
-                    std::move_backward(
-                        sa+i+j
-                      , sa+i
-                      , sa+i+1);
-                sa[i+j] = EMPTY;
+                {
+                    std::move_backward(sa+i+j, sa+i, sa+i+1);
+                    sa[i+j] = EMPTY;
+                }
             }
     }
 
-    template<class SEQ_ITR, class SA_ITR>
+    template<class SEQ_ITR>
     void put_suffix1(
         const SEQ_ITR seq
-      , SA_ITR sa
-      , SaIndex<SA_ITR> n1
+      , SaItr sa
+      , Index n1
     )
     {
-        std::size_t pos, cur, pre = -1;
+        Index pos, cur, pre = -1;
         for (auto i = n1-1; i > 0; i--)
         {
             auto j = sa[i];
--- unit_test/saca_k_test.cpp
+++ unit_test/saca_k_test.cpp
@@ -1,12 +1,68 @@
 #include <gtest/gtest.h>
 #include <random>
 #include <chrono>
+#include <algorithm>
 #include "saca_k.hpp"
 
+// The test case is designed to reach no recursion
+TEST(SACA_KTEST, RecursionLevel0)
+{
+    std::string seq {"bananaa"}; // include $
+    std::vector<uint32_t> ans_sa {6, 5, 3, 1, 0, 4, 2};
+    std::vector<uint32_t> sa(ans_sa.size());
+
+    std::transform(seq.begin(), seq.end(), seq.begin(),
+        [](char base) 
+        {
+            switch (base)
+            {
+                case 'a': return 0;
+                case 'b': return 1;
+                case 'n': return 2;
+                default: 
+                    throw std::runtime_error(
+                        "unknown character, ascii code: " 
+                      + std::to_string(base));
+            }
+        });
+
+    SACA_K<decltype(seq), decltype(sa)> sa_builder;
+    sa_builder.build(seq, sa, 3);
+    EXPECT_EQ(ans_sa, sa);
+}
+
+// The test case is designed to reach 1 recursion
+TEST(SACA_KTEST, RecursionLevel1)
+{
+    std::string seq {"banaananana"}; // include $
+    std::vector<uint32_t> ans_sa {10, 3, 8, 1, 6, 4, 0, 9, 2, 7, 5}; 
+    std::vector<uint32_t> sa(ans_sa.size());
+
+    std::transform(seq.begin(), seq.end(), seq.begin(),
+        [](char base) 
+        {
+            switch (base)
+            {
+                case 'a': return 0;
+                case 'b': return 1;
+                case 'n': return 2;
+                default: 
+                    throw std::runtime_error(
+                        "unknown character, ascii code: " 
+                      + std::to_string(base));
+            }
+        });
+
+    SACA_K<decltype(seq), decltype(sa)> sa_builder;
+    sa_builder.build(seq, sa, 3);
+    EXPECT_EQ(ans_sa, sa);
+}
+
+// A more complecate and complete test case
 TEST(SACA_KTest, BasicTest)
 {
     std::string seq{"TAAAGGGGCCCCCCAATATAATTTTGGGGCAAAGGGGCCCCCCAATAATTTTGGGGCAATAAAAAAATTTTTA"}; // the extra A denote $
-    std::vector<std::size_t> ans_sa{72, 60, 61, 62, 63, 30, 1, 64, 31, 2, 57, 43, 14, 19, 46, 65, 32, 3, 58, 17, 44, 15, 20, 47, 66, 29, 56, 42, 13, 41, 12, 40, 11, 39, 10, 38, 9, 37, 8, 28, 55, 36, 7, 27, 54, 35, 6, 26, 53, 34, 5, 25, 52, 33, 4, 71, 59, 0, 18, 45, 16, 24, 51, 70, 23, 50, 69, 22, 49, 68, 21, 48, 67};
+    std::vector<uint32_t> ans_sa{72, 60, 61, 62, 63, 30, 1, 64, 31, 2, 57, 43, 14, 19, 46, 65, 32, 3, 58, 17, 44, 15, 20, 47, 66, 29, 56, 42, 13, 41, 12, 40, 11, 39, 10, 38, 9, 37, 8, 28, 55, 36, 7, 27, 54, 35, 6, 26, 53, 34, 5, 25, 52, 33, 4, 71, 59, 0, 18, 45, 16, 24, 51, 70, 23, 50, 69, 22, 49, 68, 21, 48, 67};
 
     std::transform(seq.begin(), seq.end(), seq.begin(),
         [](char base) 
@@ -24,34 +80,34 @@ TEST(SACA_KTest, BasicTest)
             }
         });
 
-    SACA_K sorter;
-    std::vector<std::size_t> sa(ans_sa.size());
-    sorter(seq, sa, 4);
+    int alphabet_size = 4; // A T G C
+    std::vector<uint32_t> sa(ans_sa.size());
+    SACA_K<decltype(seq), decltype(sa)> sa_builder;
+    sa_builder.build(seq, sa, alphabet_size);
     EXPECT_EQ(ans_sa, sa);
 }
 
-TEST(SACA_KTest, PerformanceTest)
+TEST(SACA_KTest, PerformanceTest1MB)
 {
-    std::size_t length = 1024*1024;
-    std::string seq;
-    seq.reserve(length);
-    std::vector<std::size_t> sa(length);
-    SACA_K sorter;
+    uint32_t length = 1024*1024;
+    std::vector<char> seq(length);
+    std::vector<uint32_t> sa(length);
+    SACA_K<decltype(seq), decltype(sa)> sa_builder;
 
     // generate radom DNA sequence 
     // {'A', 'C', 'G', 'T'} => {0, 1, 2, 3};
     std::default_random_engine eng;
-    std::uniform_int_distribution<int> dist(0, 3); 
-    for(std::size_t i = 0; i < length; i ++) {
-        seq.push_back(dist(eng));
-    }
-    seq.push_back(0); // $
+    std::uniform_int_distribution<char> dist(0, 3); 
+    auto get_random_base = [&eng, &dist]() { return dist(eng); };
+    // seq[length] denote $
+    std::generate(seq.begin(), seq.end()-1, get_random_base);
 
     auto start = std::chrono::high_resolution_clock::now();
-    sorter(seq, sa, 4);
+    sa_builder.build(seq, sa, 4);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
     std::cerr << "Random DNA seq size: 1MB\n"
               << "Suffix array construction time: " 
               << elapsed.count() << "s\n";
 }
+
